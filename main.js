a326358f39b33175b8523512f45b033e1e4db99d
rhit-annavaa <annavaa@rose-hulman.edu>
2024-09-23 14:43:46 -0400

    GPE Car model in with polish
    The GPE model is now in there with the DracoLoader from yesterday. I just cleaned up/commented on the JS and got rid off CSS. HTML already looks pretty good.

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+//necessary import for GPE model for some reason, something about 3d meshes?
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-
+//renderer creation and color space decleration
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
+//create renderer properties
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -17,21 +18,22 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 document.body.appendChild(renderer.domElement);
 
+//scene creation 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(4, 5, 11);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enablePan = false;
+// controls.enableDamping = true;
+// controls.enablePan = false;
 controls.minDistance = 5;
 controls.maxDistance = 20;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
-controls.autoRotate = false;
-controls.target = new THREE.Vector3(0, 1, 0);
-controls.update();
+// controls.autoRotate = false;
+// controls.target = new THREE.Vector3(0, 1, 0);
+// controls.update();
 
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
@@ -54,16 +56,16 @@ scene.add(spotLight);
 
 
 
-//ORIGINAL JS
+//make loader
 const loader = new GLTFLoader();
-// loader.setPath('public/gpe_testone/');
+//make dracoloader for meshes or whatever and set decoder location to that addy
 const draco = new DRACOLoader();
 draco.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-// draco.setDecoderPath("/draco");
+// make the standar loader a draco loader
 loader.setDRACOLoader(draco);
 
 loader.load( 'public/gpe_testone/scene.gltf', function ( gltf ) {
-  gltf.scene.rotation.x -= 1.5;
+  gltf.scene.rotation.x -= 1.55;
   // gltf.scene.rotation.y += 0.1;
 
 	scene.add( gltf.scene );
@@ -74,45 +76,22 @@ loader.load( 'public/gpe_testone/scene.gltf', function ( gltf ) {
 
 } );
 
-// loader.load('aerodynamics_-_cfd_model_(2)_(1).gltf', (gltf) => {
-//   console.log('loading model');
-//   const mesh = gltf.scene;
-
-//   mesh.traverse((child) => {
-//     if (child.isMesh) {
-//       child.castShadow = true;
-//       child.receiveShadow = true;
-//     }
-//   scene.add( gltf.scene );
-
-//   });
-
-//   mesh.position.set(0, 1.05, -1);
-//   scene.add(mesh);
-
-//   function createMesh(geometry, material, x, y, z, name){
-//     const mesh = new THREE.Mesh(geometry, material.clone());
-//     mesh.position.set(x,y,z);
-//     mesh.name= name;
-//     mesh.castShadow = true;
-//     mesh.receiveShadow = true;
-//     return mesh;
-//   }
+  function createMesh(geometry, material, x, y, z, name){
+    const mesh = new THREE.Mesh(geometry, material.clone());
+    mesh.position.set(x,y,z);
+    mesh.name= name;
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    return mesh;
+  }
 
 //   //adding more stuff for testing clicks
 
-//   const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 2);
-//   const material = new THREE.MeshLambertMaterial();
-//   const cylinders = new THREE.Group();
-//   cylinders.add(createMesh(cylinderGeometry, material, 3, 1, 0, 'Cylinder'));
-//   scene.add(cylinders);
-
-//   document.getElementById('progress-container').style.display = 'none';
-// }, (xhr) => {
-//   console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
-// }, (error) => {
-//   console.error(error);
-// });
+  const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 2);
+  const material = new THREE.MeshLambertMaterial();
+  const cylinders = new THREE.Group();
+  cylinders.add(createMesh(cylinderGeometry, material, 3, 1, 0, 'Cylinder'));
+  scene.add(cylinders);
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -126,7 +105,7 @@ function animate() {
   renderer.render(scene, camera);
 }
 
-//Raycasting setup attempt
+//Raycasting setup attempt (detecting mouse clicks)
 const raycaster = new THREE.Raycaster();
 
 document.addEventListener('mousedown', onMouseDown);
